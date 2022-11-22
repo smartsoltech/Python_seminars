be6aa625a654f053008e78fddd19e8e84eae8b12
Andrew Choi <a.choi@smartsoltech.kr>
2022-11-22 19:59:38 +0900

    Task2 Solved

--- Homework/Seminar1/Task2.py
+++ Homework/Seminar1/Task2.py
@@ -1,9 +1,5 @@
-#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-x = int(input("X >> "))
-y = int(input("Y >> "))
-z = int(input("Z >> "))
-
-if (not (x or y or z) == (not x)):
-   print ("kolsdifhpaodfjh23") 
-else:
-    print("2903420394823")
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
